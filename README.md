40a780991104e5c4b21ace7cbcf6d5a4067a92ee
Saiteja0345 <164837660+Saiteja0345@users.noreply.github.com>
2024-10-22 01:48:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dynamic-portfolio-website
+
+# Screenshot
+<img align="left" alt="screenshot" height="500px" width="100%" src="https://github.com/Madhuredra/dynamic-portfolio-website/blob/master/portfolio.png" />
+# Full-Stack-Development-Project
---
+++ portfolio-frontend-react/src/components/User/Footer/Footer.js
@@ -0,0 +1,147 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const Footer = ({ onLinkClick }) => {
+  return (
+    <footer className="foot text-white pt-5 pb-4">
+      <div className="container text-center text-md-left">
+        <div className="row text-center text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold title">
+              Sri Kundu
+            </h5>
+            <p className="sub-title">
+              I am an engineering student in the 4th year of Artificial Intelligence
+              at the Amrita Vishwa Vidyapeetham.
+            </p>
+          </div>
+
+          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold title">
+              Links
+            </h5>
+            <p className="link-hover">
+              <Link
+                onClick={() => onLinkClick("educationSection")}
+                className="sub-title"
+                style={{ textDecoration: "none" }}
+                to="/"
+              >
+                {" "}
+                Education
+              </Link>
+            </p>
+            <p className="link-hover">
+              <Link
+                onClick={() => onLinkClick("experienceSection")}
+                to="/"
+                className="sub-title"
+                style={{ textDecoration: "none" }}
+              >
+                Experience
+              </Link>
+            </p>
+
+            <p className="link-hover">
+              <Link
+                onClick={() => onLinkClick("projectSection")}
+                to="/"
+                className="sub-title"
+                style={{ textDecoration: "none" }}
+              >
+                Projects
+              </Link>
+            </p>
+            <p className="link-hover">
+              <Link
+                onClick={() => onLinkClick("skillSection")}
+                to="/"
+                className="sub-title"
+                style={{ textDecoration: "none" }}
+              >
+                Skills
+              </Link>
+            </p>
+          </div>
+
+          <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 sub-title">
+            <h5 className="text-uppercase mb-4 font-weight-bold title">
+              Contact
+            </h5>
+            <p>Tadepalligudem, Andhra Pradesh , India</p>
+            <p>bannukundula345@gmail.com</p>
+            <p>+919398108203</p>
+          </div>
+        </div>
+
+        <hr className="mb-4" />
+
+        <div className="row align-items-center">
+          <div className="col-md-7 col-lg-8">
+            <p>
+              {" "}
+              Copyright ©2024 All rights reserved by:
+              <span style={{ textDecoration: "none" }}>
+                <strong className="title">Sri Kundu</strong>
+              </span>
+            </p>
+          </div>
+
+          <div className="col-md-5 col-lg-4">
+            <div className="text-center text-md-right">
+              <ul className="list-unstyled list-inline">
+                <li className="list-inline-item">
+                  <a
+                    href="https://www.instagram.com/intellectual.shit"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <i className="fa-brands fa-instagram"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://twitter.com/intellectual.shit"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <i className="fab fa-twitter icons-color"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://www.linkedin.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <i className="fab fa-linkedin-in icons-color"></i>
+                  </a>
+                </li>
+                <li className="list-inline-item">
+                  <a
+                    href="https://github.com/madhuredra"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn-floating btn-sm text-white"
+                    style={{ fontSize: "23px" }}
+                  >
+                    <i className="fab fa-github icons-color"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
